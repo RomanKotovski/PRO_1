c7d4b9fdb8e8936d26a45602c0fe3a7fc8e5e1cd
Roman_Kotovski <roman_kotovski@mail.ru>
2023-06-06 18:35:41 +0300

    Initial commit

---
+++ application/db/people.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+from emoji import emojize
+
+def get_employees():
+    print(f'Работников тоже нет{emojize("💩")}\n', datetime.now())
+
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+from emoji import emojize
+
+def calculate_salary():
+    print(f'Зарплаты нет{emojize("😈")}\n', datetime.now())
+
